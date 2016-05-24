9d957e14fa74963ae1f7748d6fe75694e2b58ab4
bekahdevore <devore.rc@gmail.com>
2016-05-24 11:57:09 -0400

    changed variable name

--- PUMS.ForeignBorn.R
+++ PUMS.ForeignBorn.R
@@ -23,7 +23,7 @@ MSA <- c(2101701, 2101702, 2101703, 2101704, 2101705, 2101706, 2101600, 2101800,
         filter(FPUMA %in% MSA) %>% ## filter to Louisville MSA (approximate)
         filter(AGEP >= 25) %>% ## filter to pop 25 and over
         filter(NATIVITY>1) %>% ## filter to foreign born
-        select(SERIALNO, NATIVITY, NOP, OCCP, POBP, WAGP, SEMP, SOCP, SPORDER, PUMA, PWGTP, AGEP, ESR, SCHL) ## keep needed variables only
+        select(SERIALNO, NATIVITY, NOP, OCCP, POBP, WAGP, SEMP, SOCP, SPORDER, FPUMA, PWGTP, AGEP, ESR, SCHL) ## keep needed variables only
 ) -> data
 
 summary(data) ## check to see if filters worked
