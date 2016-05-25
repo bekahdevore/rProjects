42b4d46d694e423618a7e2690eafac529a79d87b
bekahdevore <devore.rc@gmail.com>
2016-05-25 17:47:13 -0400

    cleaned

--- 100people.R
+++ 100people.R
@@ -12,9 +12,9 @@ library(dplyr)
 ############################ LOAD DATA #########################################
 ################################################################################
 
-## read in combined Kentucky and Indiana pums file
-kentucky <- read.csv("~/../Desktop/pumsKy.csv")
-indiana <- read.csv("~/../Desktop/pumsIn.csv")
+# read in combined Kentucky and Indiana pums file
+kentucky <- read.csv("~/../Desktop/rProjects/100people/pumsKy.csv")
+indiana <- read.csv("~/../Desktop/rProjects/100people/pumsIn.csv")
 
 ##Combine data sets
 kyIn <- rbind(kentucky, indiana)
@@ -22,11 +22,16 @@ kyIn <- rbind(kentucky, indiana)
 ############################ FUNCTIONS #########################################
 ################################################################################
 
-##Percentage "n" in df  
+##Percentage of total function for n
 percent <- function(data){
         data <-  data %>% mutate(per=n/sum(n))
         print(data)
 }
+
+##countWeight <- function(y, z){
+##   count(y, z, wt = PWGTP, sort = FALSE)
+##}
+
 
 ############################ MAIN DATA FILTER ##################################
 ################################################################################
@@ -37,99 +42,77 @@ pums14 <- c(2101701, 2101702, 2101703, 2101704, 2101705, 2101706, 2101600, 21018
 workforce <- (kyIn %>%
                       filter(FPUMA %in% pums14) %>% ## filter to Louisville MSA (approximate)
                       filter(AGEP >= 16) %>% ## filter to pop 16 and over
-                      select(FPUMA, ESR, AGEP, PWGTP, WAGP, SCHL)) ## Select variables of interest 
+                      select(FPUMA, ESR, AGEP, PWGTP, WAGP, SCHL, MANU)) ## Select variables of interest 
 
 ####################################################################################
 ############################ 100 PEOPLE COUNTDOWN BEGINS ###########################
 ####################################################################################
 
 ############################ Labor Force ###########################
-##filter for labor force
-##Recode ESR data
-laborforce <- (workforce %>%
-                       mutate(Emp = ifelse(ESR == 1|ESR == 2 |ESR == 3, 1, #In Labor Force = 1
-                                           ifelse(ESR == 6, 2, 3) #Not in Labor Force =2, other= 3
-                       ))%>%
-                       filter(Emp < 3)
-)
 
-##calulcate number then percent in labor force, not in labor force
+laborforce <- (workforce %>%
+                       mutate(Emp =  ifelse(ESR == 1|ESR == 2 |ESR == 3, "In Labor Force", 
+                                            ifelse(ESR == 6, "Not in Labor Force", 3)))%>% 
+                       filter(Emp != 3))
+
 x <- count(laborforce, Emp, wt= PWGTP, sort = FALSE) #weight by PWGTP
 percent(x)
-print("1 = in Labor Force; 2 = Not in Labor Force")
 
 
 ############################ Employed ##############################
-##of Labor Force - Employed 
+## in Labor Force - Employed 
 employed <- (workforce %>%
-                     mutate(Emp = ifelse(ESR == 1|ESR == 2, 1, 
-                                         ifelse(ESR == 3, 2, 3)
-                     ))%>%
-                     filter(Emp < 3)
-)
-e <- count(employed, Emp, wt= PWGTP, sort = FALSE) #weight by PWGTP
+                     mutate(Emp = ifelse(ESR == 1|ESR == 2, "Employed", 
+                                         ifelse(ESR == 3, "Unemployed", 3)))%>%
+                     filter(Emp != 3))
+
+e <- count(employed, Emp, wt=PWGTP, sort = FALSE)
 percent(e)
-print("1 = Employed; 2 = Unemployed")
 
 
 ############################ Employed Earning more than $28,974 ##############################
 ##Note*  $28,974 represents (25th pct wage production + install,maint,repair occupations), simulating 
 ##wage for entry level jobs in specific areas of manufacturing 
+## employed earning less than x 
 
 earning <- (workforce %>%
                     mutate(Emp = ifelse(ESR == 1|ESR == 2, 1, 
-                                        ifelse(ESR == 3, 2, 3)
-                    ))%>%
+                                        ifelse(ESR == 3, 2, 3)  ))%>%
                     filter(Emp < 2) %>%
-                    mutate(Earn = ifelse(WAGP > 28974.4, 1, 2) )
-)
+                    mutate(Earn = ifelse(WAGP > 28974.4, "Earns more", "Earns less than or equal to $28,974")))
+
 earns <- count(earning, Earn, wt= PWGTP, sort = FALSE) #weight by PWGTP
 percent(earns)
-print("1 = earns more than $28,974; 2 = earns <= $28,974")
 
-############################ Educational Attainment ##############################
-## less than a bachelors of employed and unemployed making less than $28,974
+
+############################ EDUCATIONAL ATTAINMENT ##############################
+## in Labor market, employed and unemployed, earning less than $28,974,  with less than a bachlor's degree
 
 education <- (workforce %>%
                       mutate(Emp = ifelse(ESR == 1|ESR == 2, 1, 
-                                          ifelse(ESR == 3, 2, 3)
-                      ))%>%
+                                          ifelse(ESR == 3, 2, 3))) %>%
                       filter(Emp < 3) %>%
                       mutate(Earn = ifelse(WAGP > 28974.4, 1, 2))%>%
-                      mutate(Edu = ifelse(SCHL >= 21, 1, 2))%>%
-                      filter(Earn>1)
-)
+                      mutate(Edu = ifelse(SCHL >= 21, "Bachelors or above", "Less than Bachelors"))%>%
+                      filter(Earn>1))
+
 
 ed <- count(education, Edu, wt= PWGTP, sort = FALSE) #weight by PWGTP
 percent(ed)
-print("1 = bachelor's degree or above ; 2 = less than bachelor's degree")
-
-############################ Working in Manufacturing ##############################
-## less than a bachelors of employed and unemployed
-########### Working in Manufacturing (take away all)
-
-########### Female (take away 3/4)
-########### Disablity
-########### Age
-########### English
 
 
+############################ MANUFACTURING ##############################
+## in Labor market, employed and unemployed, earning less than $28,974,  
+##with less than a bachlor's degree, not working in manufacturing
 
+manufacturing <- (workforce %>%
+                          mutate(Emp = ifelse(ESR == 1|ESR == 2, 1, 
+                                              ifelse(ESR == 3, 2, 3)))%>%
+                          filter(Emp < 3) %>%
+                          mutate(Earn = ifelse(WAGP > 28974.4, 1, 2))%>%
+                          mutate(Edu = ifelse(SCHL >= 21, 1, 2))%>%
+                          filter(Earn>1)%>%
+                          mutate(manu = ifelse(MANU == 3, "In Manufacturing", "Not in Manufacturing" )))
 
-
-
-
-
-
-
-
-
-
-
-
-
-##Using filter for those in labor force
-##Calculate percent employed
-
-##Using percent filter for those in labor force
-##
+Manufa <- count(manufacturing, manu, wt= PWGTP, sort = FALSE) #weight by PWGTP
+percent(Manufa)
