1119ebce5a08f9b18af451bc3f44560eeb0346c4
bekahdevore <devore.rc@gmail.com>
2016-05-26 12:46:21 -0400

    finished all sections

--- 100people/100people.r
+++ 100people/100people.r
@@ -42,77 +42,87 @@ pums14 <- c(2101701, 2101702, 2101703, 2101704, 2101705, 2101706, 2101600, 21018
 workforce <- (kyIn %>%
                       filter(FPUMA %in% pums14) %>% ## filter to Louisville MSA (approximate)
                       filter(AGEP >= 16) %>% ## filter to pop 16 and over
-                      select(FPUMA, ESR, AGEP, PWGTP, WAGP, SCHL, MANU)) ## Select variables of interest 
-
-####################################################################################
-############################ 100 PEOPLE COUNTDOWN BEGINS ###########################
-####################################################################################
-
-############################ Labor Force ###########################
-
-laborforce <- (workforce %>%
-                       mutate(Emp =  ifelse(ESR == 1|ESR == 2 |ESR == 3, "In Labor Force", 
-                                            ifelse(ESR == 6, "Not in Labor Force", 3)))%>% 
-                       filter(Emp != 3))
-
-x <- count(laborforce, Emp, wt= PWGTP, sort = FALSE) #weight by PWGTP
-percent(x)
+                      select(FPUMA, ESR, AGEP, PWGTP, WAGP, SCHL, MANU, SEX, AGEP, DIS, ENG)) ## Select variables of interest 
+########### Age                    
+age <- (workforce %>%
+                mutate(Emp = ifelse(ESR == 1|ESR == 2, 1, 
+                                    ifelse(ESR == 3, 2, 3)))%>%
+                filter(Emp < 3) %>%
+                mutate(Earn = ifelse(WAGP > 28974.4, 1, 2))%>%
+                mutate(Edu = ifelse(SCHL >= 21, 1, 2))%>%
+                filter(Earn>1)%>%
+                mutate(manu = ifelse(MANU == 3, "In Manufacturing", "Not in Manufacturing"))%>%
+                filter(MANU != 3) %>% #keep participants not already working in manufacturing
+                mutate(a = ifelse(AGEP >= 55, "55 +", "Under 55")))
 
 
-############################ Employed ##############################
-## in Labor Force - Employed 
-employed <- (workforce %>%
-                     mutate(Emp = ifelse(ESR == 1|ESR == 2, "Employed", 
-                                         ifelse(ESR == 3, "Unemployed", 3)))%>%
-                     filter(Emp != 3))
 
-e <- count(employed, Emp, wt=PWGTP, sort = FALSE)
-percent(e)
+ageCount <- count(age, a, wt= PWGTP, sort = FALSE) #weight by PWGTP
+percent(ageCount) 
 
 
-############################ Employed Earning more than $28,974 ##############################
-##Note*  $28,974 represents (25th pct wage production + install,maint,repair occupations), simulating 
-##wage for entry level jobs in specific areas of manufacturing 
-## employed earning less than x 
 
-earning <- (workforce %>%
+########### Disablity
+
+disability <- (workforce %>%
+                       mutate(Emp = ifelse(ESR == 1|ESR == 2, 1, 
+                                           ifelse(ESR == 3, 2, 3)))%>%
+                       filter(Emp < 3) %>%
+                       mutate(Earn = ifelse(WAGP > 28974.4, 1, 2))%>%
+                       mutate(Edu = ifelse(SCHL >= 21, 1, 2))%>%
+                       filter(Earn>1)%>%
+                       filter(MANU != 3) %>% #keep participants not already working in manufacturing
+                       filter(AGEP < 55)%>% #Keep participants under 55
+                       mutate(d = ifelse(DIS == 1, "With a disablity", "Without a disability")))
+
+dis <- count(disability, d, wt= PWGTP, sort = FALSE) #weight by PWGTP
+percent(dis)
+
+
+########### English
+
+
+english <- (workforce %>%
                     mutate(Emp = ifelse(ESR == 1|ESR == 2, 1, 
-                                        ifelse(ESR == 3, 2, 3)  ))%>%
-                    filter(Emp < 2) %>%
-                    mutate(Earn = ifelse(WAGP > 28974.4, "Earns more", "Earns less than or equal to $28,974")))
+                                        ifelse(ESR == 3, 2, 3)))%>%
+                    filter(Emp < 3) %>%
+                    mutate(Earn = ifelse(WAGP > 28974.4, 1, 2))%>%
+                    mutate(Edu = ifelse(SCHL >= 21, 1, 2))%>%
+                    filter(Earn>1)%>%
+                    filter(MANU != 3) %>% #keep participants not already working in manufacturing
+                    filter(AGEP < 55)%>% #Keep participants under 55
+                    filter(DIS == 2)%>%
+                    filter(ENG >=1 )%>%
+                    mutate(e = ifelse(ENG == 1 | ENG == 2, "Speaks enlish well",
+                                      ifelse(ENG == 3 | ENG == 4, "Does not speak english well", 3 ))))
 
-earns <- count(earning, Earn, wt= PWGTP, sort = FALSE) #weight by PWGTP
-percent(earns)
+eng <- count(english, e, wt= PWGTP, sort = FALSE) #weight by PWGTP
+percent(eng)
 
-
-############################ EDUCATIONAL ATTAINMENT ##############################
-## in Labor market, employed and unemployed, earning less than $28,974,  with less than a bachlor's degree
-
-education <- (workforce %>%
-                      mutate(Emp = ifelse(ESR == 1|ESR == 2, 1, 
-                                          ifelse(ESR == 3, 2, 3))) %>%
-                      filter(Emp < 3) %>%
-                      mutate(Earn = ifelse(WAGP > 28974.4, 1, 2))%>%
-                      mutate(Edu = ifelse(SCHL >= 21, "Bachelors or above", "Less than Bachelors"))%>%
-                      filter(Earn>1))
-
-
-ed <- count(education, Edu, wt= PWGTP, sort = FALSE) #weight by PWGTP
-percent(ed)
-
-
-############################ MANUFACTURING ##############################
+############################ FEMALE ##############################
 ## in Labor market, employed and unemployed, earning less than $28,974,  
-##with less than a bachlor's degree, not working in manufacturing
+##with less than a bachlor's degree, not working in manufacturing,
+## + percent male/female 
 
-manufacturing <- (workforce %>%
-                          mutate(Emp = ifelse(ESR == 1|ESR == 2, 1, 
-                                              ifelse(ESR == 3, 2, 3)))%>%
-                          filter(Emp < 3) %>%
-                          mutate(Earn = ifelse(WAGP > 28974.4, 1, 2))%>%
-                          mutate(Edu = ifelse(SCHL >= 21, 1, 2))%>%
-                          filter(Earn>1)%>%
-                          mutate(manu = ifelse(MANU == 3, "In Manufacturing", "Not in Manufacturing" )))
+sx <- (workforce %>%
+               mutate(Emp = ifelse(ESR == 1|ESR == 2, 1, 
+                                   ifelse(ESR == 3, 2, 3)))%>%
+               filter(Emp < 3) %>%
+               mutate(Earn = ifelse(WAGP > 28974.4, 1, 2))%>%
+               mutate(Edu = ifelse(SCHL >= 21, 1, 2))%>%
+               filter(Earn>1)%>%
+               filter(MANU != 3) %>% #keep participants not already working in manufacturing
+               filter(DIS == 2)%>%
+               filter(ENG >=3)%>%
+               mutate(s = ifelse(SEX == 1, "Male", 
+                                 ifelse(SEX == 2, "Female", "NA")))
+)
 
-Manufa <- count(manufacturing, manu, wt= PWGTP, sort = FALSE) #weight by PWGTP
-percent(Manufa)
+
+
+sex <- count(sx, s, wt= PWGTP, sort = FALSE) #weight by PWGTP
+percent(sex)
+
+########### Female (take away 3/4)
+t = sex[1,2]/4 ##1/4 of females
+print(t)
